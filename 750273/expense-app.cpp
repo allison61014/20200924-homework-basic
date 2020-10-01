ad57a66e248e5f7888449a2ac10c38d92d331fa1
allison61014 <allison61014@gmail.com>
2020-10-01 22:17:57 +0800

    新增 750273 的作業

---
+++ 750273/expense-app.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int count = 0;
+    int money = 0;
+    int sum = 0;
+
+    cout << "請問有幾筆資料？\n";
+    cin >> count;
+
+    for (int i = 1; i <= count; i++) {
+        cout << "第" << i << "筆：";
+        cin >> money;
+        sum += money;
+    } 
+    cout << "總共" << sum << "元";
+}
---
+++ 750273/mario.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int height;
+
+    cout << "高度：";
+    cin >> height;
+
+    for (int i = 0; i <= height; i++) {
+        for (int j = height; j >= i; j--) {
+            cout << " ";
+        }
+        for (int k = 0; k <= i; k++) {
+            cout << "#";
+        }
+        cout << "\n";
+   }
+}
